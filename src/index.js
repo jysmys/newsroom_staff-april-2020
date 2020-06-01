c275d3533f9769c36c35a9a22dc885593d5765f6
japh <jenny.scherr@svenskaspel.se>
2020-06-01 11:57:32 +0200

    update axios path

--- src/index.js
+++ src/index.js
@@ -9,9 +9,9 @@ import "./css/index.css";
 import { Provider } from "react-redux";
 import configureStore from "./state/store/configureStore";
 
-axios.defaults.baseURL = "http://localhost:3000/api";
+axios.defaults.baseURL = "https://newsroom-api.herokuapp.com/api";
 const store = configureStore();
-window.store = store
+window.store = store;
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
